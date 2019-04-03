8155be8b2098ea8626409c596c42a91cc7dc1098
NatpakalJAM <jam13432@gmail.com>
2019-04-03 23:51:53 +0700

    update get attachments

--- prayut.js
+++ prayut.js
@@ -22,13 +22,25 @@ botPrayut.on('ready', () => {
 botPrayut.on('messageUpdate', (oldMessage, newMessage) => {
     if (newMessage.author.bot == true) return; // ถ้าเป็นbot
     if (newMessage.author.id !== botConfig.PrayutID) return; // ถ้าไม่ใช่ประยุทธ
-    newMessage.guild.channels.get(botConfig.ChannelID).send('```ข้อความเก่า: ' + oldMessage.content + '\nข้อความใหม่: ' + newMessage.content + '```');
+    newMessage.guild.channels.get(botConfig.ChannelID).send('`ข้อความเก่า:` ' + oldMessage.content + '\n`ข้อความใหม่: `' + newMessage.content + '\n');
 });
 
 botPrayut.on('messageDelete', message => {
     if (message.author.bot == true) return; // ถ้าเป็นbot
     if (message.author.id !== botConfig.PrayutID) return; // ถ้าไม่ใช่ประยุทธ
-    message.guild.channels.get(botConfig.ChannelID).send('```ข้อความที่ลบ: ' + message.content + '```');
+    msg = message.content;
+    attachment = (message.attachments).array();
+    // attachment = attachment[''];
+    if (msg != '' && attachment.length <= 0) {
+        message.guild.channels.get(botConfig.ChannelID).send('`ข้อความที่ลบ:` ' + message.content + '\n');
+    } else if (attachment.length > 0) {
+        attachment = attachment[0].proxyURL;
+        if (msg != '') {
+            message.guild.channels.get(botConfig.ChannelID).send('`ข้อความที่ลบ:` ' + msg + '\n' + attachment + '\n');
+        } else if (msg == '') {
+            message.guild.channels.get(botConfig.ChannelID).send('`ไฟล์ที่ลบ:` ' + attachment + '\n');
+        }
+    }
 });
 
 botPrayut.login(botConfig.TOKEN);
